af20a1fcf667d045490d967f1cc8b7ebe33e1a2d
Rafael Lagisck <rafael.lagisck@exemplo.com>
2023-09-01 16:27:36 -0300

    Implementando serviços de negócios

---
+++ src/DevIO.Business/Models/Fornecedores/Services/FornecedorService.cs
@@ -0,0 +1,59 @@
+﻿using DevIO.Business.Core.Services;
+using DevIO.Business.Models.Fornecedores.Validations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Models.Fornecedores.Services
+{
+    public class FornecedorService : BaseService, IFornecedorService
+    {
+        private readonly IFornecedorRepository _fornecedorRepository;
+        private readonly IEnderecoRepository _enderecoRepository;
+
+        public FornecedorService(IFornecedorRepository fornecedorRepository, IEnderecoRepository enderecoRepository)
+        {
+            _fornecedorRepository = fornecedorRepository;
+            _enderecoRepository = enderecoRepository;
+        }
+        public async Task Adicionar(Fornecedor fornecedor)
+        {
+            if (!ExecutarValidacao(new FornecedorValidation(), fornecedor)
+                || !ExecutarValidacao(new EnderecoValidation(), fornecedor.Endereco)) return;
+
+            if (await FornecedorExistente(fornecedor)) return;
+
+            await _fornecedorRepository.Adicionar(fornecedor);
+
+            throw new NotImplementedException();
+        }
+
+        public Task Atualizar(Fornecedor fornecedor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Remover(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AtualizarEndereco(Endereco endereco)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task<bool> FornecedorExistente(Fornecedor fornecedor)
+        {
+            var fornecedorAtual = await _fornecedorRepository.Buscar(f => f.Documento == fornecedor.Documento && f.Id != fornecedor.Id);
+
+            return fornecedorAtual.Any();
+        }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
